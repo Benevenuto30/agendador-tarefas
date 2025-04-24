731de6037aa18af8cd2b85afae059a0e5bd8354c
Benevenuto30 <rafaelmaster03@gmail.com>
2025-04-24 20:26:04 -0300

    implementado client interface

--- src/main/java/com/benevenuto/agendadortarefas/AgendadorTarefasApplication.java
+++ src/main/java/com/benevenuto/agendadortarefas/AgendadorTarefasApplication.java
@@ -1,4 +1,4 @@
-package com.benevenuto.agendador_tarefas;
+package com.benevenuto.agendadortarefas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
